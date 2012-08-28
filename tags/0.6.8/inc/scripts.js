e7a3320021cb9bc1a275fc31e5193b6a97ba5d9b
yathosho <yathosho@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-08-28 10:44:30 +0000

    git-svn-id: http://plugins.svn.wordpress.org/meta-ographr@591275 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ tags/0.6.8/inc/scripts.js
@@ -0,0 +1,69 @@
+		jQuery(document).ready(function() {
+		
+				
+		
+				if (! jQuery("#show_advanced").attr('checked') ) {
+					jQuery('.advanced_opt').hide();
+				}
+				jQuery("#show_advanced").click(function(){
+					jQuery(".advanced_opt").fadeToggle('slow');
+				});
+					
+				jQuery("#enable_plugin").click(enable_cb);
+				jQuery("#attached_image").click(disable_post_thumbnail);
+				jQuery("#enable_images").click(enable_images);
+				jQuery("#enable_graph").click(enable_graph);
+				jQuery("#enable_delete").click(enable_delete);
+
+				jQuery("#enable_expiry input").click( function() {
+				    var val = parseInt( this.value );
+				    if ( val === 1 ) {
+				        jQuery("select.no_expiry, input.no_expiry").removeAttr('disabled');
+				    } else {
+				        jQuery("select.no_expiry, input.no_expiry").attr( 'disabled', 'disabled' );
+				    }
+				});
+				
+				//render stats last!
+				render_stats();		
+		});
+
+		function enable_cb() {
+			if (this.checked) {
+				jQuery("input.enable_triggers").removeAttr("disabled");
+			} else {
+				jQuery("input.enable_triggers").attr("disabled", true);
+			}
+		}
+
+		function disable_post_thumbnail() {
+			if (this.checked) {
+				jQuery("input.post_thumbnail").attr("disabled", true);
+			} else {
+				jQuery("input.post_thumbnail").removeAttr("disabled");
+			}
+		}
+	
+		function enable_images() {
+			if (this.checked) {
+				jQuery("input.disable_filters, textarea.disable_filters").removeAttr("disabled");
+			} else {
+				jQuery("input.disable_filters, textarea.disable_filters").attr("disabled", true);
+			}
+		}
+		
+		function enable_graph() {
+			if (this.checked) {
+				jQuery("input.disable_graph").removeAttr("disabled");
+			} else {
+				jQuery("input.disable_graph").attr("disabled", true);
+			}
+		}
+	
+		function enable_delete() {
+			if (this.checked) {
+				jQuery("input.enable_delete").removeAttr("disabled");
+			} else {
+				jQuery("input.enable_delete").attr("disabled", true);
+			}
+		}
