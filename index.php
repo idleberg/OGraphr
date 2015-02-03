e8d7c3f33a1220fe82d9c7ff53c144ea6442ff2b
Jan T. Sott <jan@idleberg.com>
2015-02-03 21:26:05 +0100

    removed some more debug warnings

--- index.php
+++ index.php
@@ -13,7 +13,6 @@
 
 // OGRAPHR OPTIONS
     define("OGRAPHR_VERSION", "0.8.34");
-	// enables developer settings on WordPress interface, can be overwritten from plug-in settings once activated
 	// replace default description with user agent in use
 	define("OGRAPHR_UATEST", FALSE);
 	// specify timeout for all HTTP requests (default is 1 second, http://googlecode.blogspot.co.at/2012/01/lets-make-tcp-faster.html)
@@ -348,7 +347,7 @@ class OGraphr_Core {
 		|| ((preg_match(LINKEDIN_USERAGENT, $user_agent)) && ($linkedin_ua))
 		|| ((preg_match(TWITTER_USERAGENT, $user_agent)) && ($twitter_ua))
 		|| ((!isset($facebook_ua)) && (!isset($gplus_ua)) && (!isset($linkedin_ua)) && (!isset($twitter_ua)))
-		|| ($options['debug_level'] > 0) ) {
+		|| (isset($options['debug_level'])) && ($options['debug_level'] > 0) ) {
 			// Get the post ID if none is provided
 			if($post_id==null OR $post_id=='') {
 				$post_id = get_the_ID();
@@ -541,7 +540,7 @@ class OGraphr_Core {
 
 				if ( (isset($meta_values)) && (is_array($meta_values)) && (isset($thumbnails[0]['img'])) && ($expiry >= $interval) ) {
 					$thumbnails = array_merge($thumbnails, $meta_values);
-					if( ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) {
+					if( (isset($options['debug_level'])) && ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) {
 						foreach($thumbnails as $thumbnail) {
 							if (isset($thumbnail['img'])) {
 								if (filter_var($thumbnail['img'], FILTER_VALIDATE_URL)) {
@@ -561,7 +560,7 @@ class OGraphr_Core {
 					}
 				} else if ( (isset($meta_values)) && (is_array($meta_values)) && (!isset($thumbnails)) && ($expiry >= $interval) ) {
 					$thumbnails = $meta_values;
-					if( ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) {
+					if( (isset($options['debug_level'])) && ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) {
 						foreach($thumbnails as $thumbnail) {
 							if(isset($thumbnail['img'])){
 								if(filter_var($thumbnail['img'], FILTER_VALIDATE_URL)) {
@@ -598,7 +597,7 @@ class OGraphr_Core {
 					
 					//write to db for future use
 					if (($options['exec_mode'] == 1) && ($total_img >= 1)) {
-						if( ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) {
+						if( (isset($options['debug_level'])) && ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) {
 							print "\n\t New data indexed and written to database ($total_img)\n";
 						}
 						
@@ -1141,7 +1140,7 @@ class OGraphr_Core {
 				}
 				
 				// Print Twitter Cards
-				if ((isset($options['add_twitter_meta'])) && ((preg_match(TWITTER_USERAGENT, $user_agent)) || ( ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) )) {
+				if ((isset($options['add_twitter_meta'])) && ((preg_match(TWITTER_USERAGENT, $user_agent)) || ( (isset($options['debug_level'])) && ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) )) {
 					foreach($twitter_meta as $key => $value) {
 						print "<meta name=\"$key\" content=\"$value\" />\n";
 					}
@@ -1149,7 +1148,7 @@ class OGraphr_Core {
 				}
 
 				// Print Google+ Meta
-				if ((isset($options['add_google_meta'])) && ((preg_match(GOOGLEPLUS_USERAGENT, $user_agent)) || ( ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) )))
+				if ((isset($options['add_google_meta'])) && ((preg_match(GOOGLEPLUS_USERAGENT, $user_agent)) || ( (isset($options['debug_level'])) && ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) )))
 					foreach($google_meta as $key => $value) {
 						print "<meta name=\"$key\" content=\"$value\" />\n";
 					}
@@ -1174,7 +1173,7 @@ class OGraphr_Core {
 		if (isset($options['add_post_images'])) {
 			preg_match_all('/<img.+?src=[\'"]([^\'"]+)[\'"].*?>/i', $markup, $matches);
 			foreach($matches[1] as $match) {
-			  	if(( ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && (is_single()) || (is_front_page())) {
+			  	if(( (isset($options['debug_level'])) && ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && (is_single()) || (is_front_page())) {
 					print "\t Image tag: $match\n";
 				}
 			
@@ -1258,7 +1257,7 @@ class OGraphr_Core {
 			foreach($matches[1] as $match) {
 				$match = preg_replace('/^\/\/+?/', 'http://', $match); // fix Viddler thumbnail URL
 				$match = $this->ographr_rel2abs($match, $blog_url);
-			  	if( ( ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && (is_single()) || (is_front_page()) ) {
+			  	if( ( (isset($options['debug_level'])) && ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && (is_single()) || (is_front_page()) ) {
 					print "\t Video poster: $match\n";
 				}
 			
@@ -1283,12 +1282,12 @@ class OGraphr_Core {
 				if ($options['exec_mode'] == 1)  {
 					$exists = $this->remote_exists($website_thumbnail);
 					if(($exists) && (!$website_thumbnail))
-						if ($options['debug_level'] > 0) {
+						if ( (isset($options['debug_level'])) && ($options['debug_level'] > 0) ) {
 							print "\t Post thumbnail: $website_thumbnail\n";
 						}
 						$thumbnails[] = $website_thumbnail;
 				} else { // not sure why, think i read remote_exists is slow
-					if ($options['debug_level'] > 0) {
+					if ( (isset($options['debug_level'])) && ($options['debug_level'] > 0) ) {
 						print "\t Post thumbnail: $website_thumbnail\n";
 					}
 					$thumbnails[]['img'] = $website_thumbnail;
@@ -1303,7 +1302,7 @@ class OGraphr_Core {
 			if (isset($attached_thumbnails)) {
 				foreach ($attached_thumbnails as $attached_thumbnail) {
 					// investigate, produces error when exec_mode == 1
-					if ( ( ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && ($options['exec_mode'] == 2)) {
+					if ( ( (isset($options['debug_level'])) && ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && ($options['exec_mode'] == 2)) {
 						print "\t Attached image: $attached_thumbnail[0]\n";
 					}
 					$thumbnails[]['img'] = $attached_thumbnail[0];
@@ -1317,7 +1316,7 @@ class OGraphr_Core {
 
 			foreach($matches[1] as $match) {
 				$match = $this->ographr_rel2abs($match, $blog_url);
-				if( ( ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && (is_single()) || (is_front_page())) {
+				if( ( (isset($options['debug_level'])) && ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && (is_single()) || (is_front_page())) {
 					print "\t JW Player: $match\n";
 				}
 			
@@ -1339,7 +1338,7 @@ class OGraphr_Core {
 			preg_match_all('/(?:nvb.addVariable\([\'"]image_src[\'"],[\s])[\'"]([^\'"]+)[\'"].*?\)/smi', $markup, $matches);
 
 			foreach($matches[1] as $match) {
-				if( ( ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && (is_single()) || (is_front_page())) {
+				if( ( (isset($options['debug_level'])) && ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && (is_single()) || (is_front_page())) {
 					print "\t NVB Player: $match\n";
 				}
 			
@@ -1771,7 +1770,7 @@ class OGraphr_Core {
 			}
 			
 			// debugger output
-			if( ( ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && (is_single()) || (is_front_page())) {
+			if( ( (isset($options['debug_level'])) && ($options['debug_level'] > 0) && (current_user_can('edit_plugins')) ) && (is_single()) || (is_front_page())) {
 				if(isset($json_thumbnail['img'])){
 					print "\n\t [" . $services['name'] . "]\n";
 					print "\t ID: $match\n";
